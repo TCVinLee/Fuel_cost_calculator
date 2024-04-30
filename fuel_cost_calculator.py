d44ca53aef7b4cb21cd7475ccdc8a258f715d4d0
Vincent <91401445+TCVinLee@users.noreply.github.com>
2024-04-30 11:34:44 +0000

    Update to include instruction for convertor functionality; update to include function to check input

--- fuel_cost_calculator.py
+++ fuel_cost_calculator.py
@@ -2,16 +2,35 @@
 #per litre: 46/ 4.54609188 = 10.1185812 miles
 #1 mile = 0.0988376 litre
 
+#Set up function to check if input is float
+def check_input(prompt):
+  while True:
+    user_input = input(prompt)
+    try:
+      user_input = float(user_input)
+      return user_input
+    except ValueError:
+      print(f"It requires a number and you have entered '{user_input}'. Please enter a valid number. For example 46 or 37.85 \n\n")
+    
+
+#Print out title
 print("***** Fuel Cost Calculator *****")
-print("v1.0")
+print("********** MPG to KMpL convertor")
+print("v2.0")
 print("\n\n")
 
+#Instruction for using calculator or convertor functionality
+print("* If you only want to convert MPG to Km/L, please enter 0 for the following question *\n")
 #Enter MPG information
-mpg = float(input("Enter the number for MPG: "))
+#mpg = input("Enter the number for MPG: ")
+mpg = check_input("Enter the number for MPG: ")
 #Enter fuel cost (in pence)
-cost = float(input("Enter the cost of fuel (in pence): "))
+cost = check_input("Enter the cost of fuel (in pence): ")
 #Enter distance in miles
-distance = float(input("Enter the distance in miles: "))
+distance = check_input("Enter the distance in miles :")
+
+#Convert input to float
+#mpg = float(mpg)
 
 #Break down for miles per litre
 miles_per_litre = mpg / 4.54609188
@@ -28,5 +47,5 @@ cost_needed = litres_needed * cost
 #cost in GBP, round to 2 decimal points
 cost_in_GBP = "{:.2f}".format(cost_needed / 100)
 
-print(f"\n\n*****Cost for the trip*****\n\nCost: £{cost_in_GBP}\n\nDistance: {distance} miles | {distance_in_km} km.")
-print(f"Fuel efficiency: MPG: {mpg} = km/l: {per_litre_for_km}.")
+print(f"\n\n*****Cost for the trip*****\n\nCost: £{cost_in_GBP}\n\nDistance: {distance} miles | {distance_in_km} km.\n")
+print(f"* Fuel efficiency * \nMPG: {mpg} = km/l: {per_litre_for_km}.")
